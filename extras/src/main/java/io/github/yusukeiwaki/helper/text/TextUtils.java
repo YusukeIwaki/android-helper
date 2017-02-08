1d24d1fffd757e0233692b922729ef94f5e76756
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-02-08 16:05:57 +0900

    add :extras

---
+++ extras/src/main/java/io/github/yusukeiwaki/helper/text/TextUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017. @YusukeIwaki
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.yusukeiwaki.helper.text;
+
+/**
+ * Text Utility class like android.text.TextUtils but independent of android.* packages.
+ */
+public class TextUtils {
+
+  /**
+   * Returns true if the string is null or 0-length.
+   * @param str the string to be examined
+   * @return true if str is null or zero length
+   */
+  public static boolean isEmpty(CharSequence str) {
+    // same definition as android.text.TextUtils#isEmpty().
+    return str == null || str.length() == 0;
+  }
+
+  /**
+   * Returns str if it is not empty; otherwise defaultValue is returned.
+   */
+  public static CharSequence or(CharSequence str, CharSequence defaultValue) {
+    if (isEmpty(str)) {
+      return defaultValue;
+    }
+    return str;
+  }
+}
+
